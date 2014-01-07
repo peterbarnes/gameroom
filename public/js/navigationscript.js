c16f86a81f4b1315a699d8a3cd43af2973892252
peterbarnes <barnespeter213@gmail.com>
2014-01-07 16:53:32 -0600

    external scripts

---
+++ public/js/navigationscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.headingd').click(function() {
+    $('.hiddend').slideToggle(400);
+  }); // end click
+}); // end ready
+$(document).ready(function() {
+ $('.heading').click(function() {
+   $('.hidden').slideToggle(400);
+ }); // end click 
+}); // end ready
