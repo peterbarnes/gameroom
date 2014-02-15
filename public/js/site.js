92976c9c9fdede855e17b463f53b91f9f3406649
Peter Barnes <barnespeter213@gmail.com>
2014-02-15 17:10:18 -0600

    service

--- public/js/site.js
+++ public/js/site.js
@@ -1,14 +1,7 @@
-//contact: details hide
 $(document).ready(function() {
   $('.details').hide();
   $('.main h3').click(function() {
     $(this).toggleClass('open');
     $(this).next('.details').slideToggle(400);
-  });//end click
-});//end ready
-$(document).ready(function() {
-  $('.info').hide();
-  $('p.head').click(function() {
-    $(this).next('.info').slideToggle(400);
-  });//end click
-});//end ready
+  });
+});
